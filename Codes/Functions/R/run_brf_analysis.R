4242c2d3341ac94fa4d847af97e14f5047f82e9b
Taro Mieno <tmieno2@unl.edu>
2022-02-01 17:09:44 -0600

    modified BRF analysis by adding interaction terms

--- Codes/Functions/R/run_brf_analysis.R
+++ Codes/Functions/R/run_brf_analysis.R
@@ -5,14 +5,31 @@
 
 
 run_brf_analysis <- function(reg_data, cv_data, x_vars, pN, pCorn, N_levels) {
-  data <- copy(reg_data)
+  train_data <- copy(reg_data)
+  test_data <- copy(cv_data$data[[1]])
 
-  X <-
-    data[, c("N", x_vars), with = FALSE] %>%
-    data.frame()
+  # /*+++++++++++++++++++++++++++++++++++
+  #' ## Define X
+  # /*+++++++++++++++++++++++++++++++++++
+  int_var_names <- paste0("N_", x_vars)
+  int_var_exp <- paste0("= N * ", x_vars)
+  int_var_gen_exp <- paste0(int_var_names, int_var_exp, collapse = ",")
 
-  Y <- data[, yield]
+  all_X_vars <- c("N", x_vars, int_var_names)
 
+  eval(parse(text = paste("train_data[, `:=`(", int_var_gen_exp, ")]", sep = "")))
+  eval(parse(text = paste("test_data[, `:=`(", int_var_gen_exp, ")]", sep = "")))
+
+  X <- train_data[, ..all_X_vars]
+
+  # /*+++++++++++++++++++++++++++++++++++
+  #' ## Define Y
+  # /*+++++++++++++++++++++++++++++++++++
+  Y <- train_data[, yield]
+
+  # /*+++++++++++++++++++++++++++++++++++
+  #' ## Run BRF
+  # /*+++++++++++++++++++++++++++++++++++
   BRF_temp <-
     grf::boosted_regression_forest(
       X = X,
@@ -23,20 +40,20 @@ run_brf_analysis <- function(reg_data, cv_data, x_vars, pN, pCorn, N_levels) {
     )
 
   #************************   RMSR   ***********************
-  e_hat_train <- copy(data) %>%
-    .[, y_hat := predict(BRF_temp,
-      newdata = .[, c("N", x_vars), with = FALSE]
-    )] %>%
+  e_hat_train <-
+    train_data %>%
+    .[, y_hat := predict(BRF_temp, newdata = .[, all_X_vars, with = FALSE])] %>%
     .[, e_hat := yield - y_hat] %>%
     .[, .(aunit_id, e_hat)] %>%
     setnames("e_hat", "e_hat_train")
-  e_hat_cv <- copy(cv_data$data[[1]]) %>%
-    .[, y_hat := predict(BRF_temp,
-      newdata = .[, c("N", x_vars), with = FALSE]
-    )] %>%
+
+  e_hat_cv <-
+    test_data %>%
+    .[, y_hat := predict(BRF_temp, newdata = .[, all_X_vars, with = FALSE])] %>%
     .[, e_hat := yield - y_hat] %>%
     .[, .(aunit_id, e_hat)] %>%
     setnames("e_hat", "e_hat_cv")
+
   #*********************************************************
 
   N_seq <-
@@ -45,12 +62,10 @@ run_brf_analysis <- function(reg_data, cv_data, x_vars, pN, pCorn, N_levels) {
     )
 
   brf_results <-
-    copy(cv_data$data[[1]]) %>%
-    .[, c("aunit_id", "yield", x_vars), with = FALSE] %>%
+    test_data %>%
+    .[, c("aunit_id", "yield", all_X_vars), with = FALSE] %>%
     expand_grid_df(., N_seq) %>%
-    .[, yield_hat := predict(BRF_temp,
-      newdata = .[, c("N", x_vars), with = FALSE]
-    )] %>%
+    .[, yield_hat := predict(BRF_temp, newdata = .[, all_X_vars, with = FALSE])] %>%
     .[, pi_hat := pCorn * yield_hat - pN * N] %>%
     .[, .SD[which.max(pi_hat)], by = aunit_id] %>%
     .[, opt_N_hat := N] %>%
