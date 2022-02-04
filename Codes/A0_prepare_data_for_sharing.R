46bb5f41b8955d021fa38b79aaa1de1ae4a5bc5a
Xiaofei Li <67332764+xiaofeiagecon@users.noreply.github.com>
2022-02-04 16:11:58 -0600

    completed .R codes for data sharing

--- Codes/A0_prepare_data_for_sharing.R
+++ Codes/A0_prepare_data_for_sharing.R
@@ -6,8 +6,6 @@ rm(list = ls())
 # /*===========================================================
 #' # Preparation
 # /*===========================================================
-#* set up python environment
-library(reticulate)
 
 #* packages
 library(sp)
@@ -57,6 +55,18 @@ field_with_design <-
 
 
 # /*===========================================================
+#' # Price data (global variables)
+# /*===========================================================
+
+pCorn = 0.246063    # $/kg
+pN = 2.204624       # $/kg
+
+saveRDS(pCorn, here("Shared/Data/ToShare/pCorn.rds"))
+saveRDS(pN, here("Shared/Data/ToShare/pN.rds"))
+
+
+
+# /*===========================================================
 #' # Field map data (full field)
 # /*===========================================================
 
@@ -64,284 +74,117 @@ field_sf <-
     field_data %>% 
     filter(field_col==144) %>% 
     pull(field_sf) %>% 
-    .[[1]]
+    .[[1]] %>% 
+    data.table(.) %>% 
+    .[, col_id := NULL] %>%
+    .[, row_id := NULL] %>%
+    #---rename lat and lon---
+    setnames(
+        c("X","Y"),
+        c("lon","lat")
+    ) %>% 
+    st_as_sf()
 saveRDS(field_sf, here("Shared/Data/ToShare/field_sf.rds"))
 
 
 
 # /*===========================================================
-#' # True field parameters
+#' # Simulated regression data
 # /*===========================================================
 
-field_sf <- 
-    field_data %>% 
+#* true cell-level data
+field_pars <-
+    field_parameters %>% 
     filter(field_col==144) %>% 
-    pull(field_sf) %>% 
-    .[[1]]
-saveRDS(field_sf, here("Shared/Data/ToShare/field_sf.rds"))
+    pull(field_pars) %>% 
+    .[[1]] %>%
+    #---join aunit_id---
+    .[data.table(field_sf)[,.(cell_id, aunit_id, lat, lon)], on = "cell_id"] %>% 
+    #---true EONR---
+    .[, EONR := (pN / pCorn - b1) / (2 * b2)] %>%
+    .[, EONR := pmin(Nk, EONR)] %>%
+    .[, EONR := pmax(0, EONR)] %>% 
+    .[, c("sim", "cell_id", "lon", "lat", "EONR", "aunit_id")] %>% 
+    .[order(sim, cell_id), ]
 
+#* aggregate EONR to aunit level
+EONR_df <- field_pars %>% 
+    .[, .(EONR = mean(EONR)), by = .(sim, aunit_id)]
 
+#* aunit-level regression data
+x_vars <-
+    c(
+        "b0_1", "b0_2",
+        "Nk_2_1", "Nk_2_2", "Nk_1_1",
+        "plateau_2_1", "plateau_2_2", "plateau_1_1",
+        "theta_plateau_1", "theta_plateau_2",
+        "theta_Nk_1", "theta_Nk_2",
+        "theta_b0_1", "theta_b0_2"
+    )
+sim_data <-
+    field_with_design %>% 
+    filter(field_col==144) %>% 
+    pull(data_file_name) %>%
+    readRDS() %>%
+    pull(reg_data) %>%
+    .[[1]] %>%
+    .[, N_levels := NULL] %>%
+    unnest() %>%
+    data.table() %>%
+    .[, c("sim", "aunit_id", "X", "Y", "N", "yield",  x_vars), with = FALSE] %>%
+    setnames(
+        names(.),
+        c("sim", "aunit_id", "X", "Y", "N", "yield",
+          paste0("par", 1:(length(names(.)) - 6))
+        )
+    ) %>% 
+    #---join true EONR---
+    EONR_df[., on = c("sim", "aunit_id")]
 
-
-
-
-
-#' ReadMe:
-#' X:
-#' Y:
-#' col_id:
-#' row_id:
-#' cell_id:
-#' aunit_id:
-#' 
-
-
-
-#* cell-level true data (sprange=600 scenario)
-cell_data <- field_parameters$field_pars[[1]]
-field_dt <- field_parameters$field_sf[[1]] %>% data.table()
-cell_data <- cell_data[field_dt[, .(cell_id, aunit_id)], on = "cell_id"]
-
-#* load simulation results data
-mc_sim_results <- readRDS(file = here("Results/mc_sim_results.rds"))
-
-#* aunit-level estimated data
-est_data <-
-  mc_sim_results$sim_results[[1]] %>%
-  unnest(mc_results) %>%
-  data.table()
-
-#* price scenarios
-pCorn <- mc_sim_results$pCorn
-pN <- mc_sim_results$pN
+saveRDS(sim_data, here("Shared/Data/ToShare/sim_data.rds"))
 
 
 
 # /*===========================================================
-#' # Profitability Calculation
+#' # Read me file
 # /*===========================================================
 
-# -----------------
-# Merge data
-# -----------------
-# === merge true parameters (cell) + estimation (aunit)
-merge_data <-
-  cell_data[est_data,
-    on = c("sim", "aunit_id"),
-    allow.cartesian = TRUE
-  ]
+ReadMe <- rbind(
+    "*** field_sf: spatial map of simulated field (sf)",
+    "lon: longitude (meter)",
+    "lat: latitude (meter)",
+    "cell_id: ID for cell",
+    "aunit_id: ID for unit of analysis (2 x 3 cells)",
+    "",
+    "*** sim_data: simulated experimental data for model estimation (data.table)",
+    "sim: simulation ID",
+    "aunit_id: ID for unit of analysis (2 x 3 cells)",
+    "EONR: true economically optimal N rate",
+    "X: aunit-level longitude (meter)",
+    "Y: aunit-level latitude (meter)",
+    "N: applied N trial rate (kg/ha)",
+    "yield: realized yield (kg/ha)",
+    "par1 - par14: observed field parameters 1 - 14",
+    "",
+    "pCorn: corn price ($/kg)",
+    "pN: N fertilzier price ($/kg)"
+) %>% 
+    data.table %>% 
+    print()
+write.table(ReadMe, here("Shared/Data/ToShare/ReadMe.txt"),
+            row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-# -----------------
-# Economic analysis
-# -----------------
-econ_data <-
-  copy(merge_data) %>%
-  #--- True optima ---#
-  .[, opt_N := (pN / pCorn - b1) / (2 * b2)] %>%
-  .[, opt_N := pmin(Nk, opt_N)] %>%
-  .[, opt_N := pmax(0, opt_N)] %>%
-  .[, yield_opt := gen_yield_QP(b0, b1, b2, Nk, opt_N)] %>%
-  .[, pi_opt := pCorn * yield_opt - pN * opt_N] %>%
-  # :::: based on true response parameters
-  #--- SCAM ---#
-  .[, yield_scam := gen_yield_QP(b0, b1, b2, Nk, opt_N_scam)] %>%
-  .[, pi_scam := pCorn * yield_scam - pN * opt_N_scam] %>%
-  .[, pi_scam := pi_scam - pi_opt] %>%
-  #--- GWR ---#
-  .[, yield_gwr := gen_yield_QP(b0, b1, b2, Nk, opt_N_gwr)] %>%
-  .[, pi_gwr := pCorn * yield_gwr - pN * opt_N_gwr] %>%
-  .[, pi_gwr := pi_gwr - pi_opt] %>%
-  # :::: based on estimated gwr response parameters
-  # (i.e., using the estimated quadratic b0_hat, b1_hat, b2_hat
-  # as the true response function. Now the pi_scam_est is treated
-  # as the pi_opt.)
-  #--- SCAM ---#
-  .[, yield_scam_est := gen_yield_QD(b0_hat, b1_hat, b2_hat, opt_N_scam)] %>%
-  .[, pi_scam_est := pCorn * yield_scam_est - pN * opt_N_scam] %>%
-  #--- GWR ---#
-  .[, yield_gwr_est := gen_yield_QD(b0_hat, b1_hat, b2_hat, opt_N_gwr)] %>%
-  .[, pi_gwr_est := pCorn * yield_gwr_est - pN * opt_N_gwr] %>%
-  # :::: GWR gain over SCAM
-  #--- profit gain of gwr ---#
-  .[, pi_diff := pi_gwr - pi_scam] %>%
-  .[, pi_diff_est := pi_gwr_est - pi_scam_est]
-
-# -------------------
-# Field level profit
-# -------------------
-# take field average
-pi_data <-
-  econ_data %>%
-  .[,
-    lapply(.SD, mean),
-    by = .(sim, transfer),
-    .SDcols = c("pi_diff", "pi_diff_est", "opt_N_scam")
-  ] %>%
-  print()
-
-saveRDS(pi_data, here("Results/pi_data.rds"))
 
 # /*===========================================================
-#' # Find an illustrative sim case for overestimation
-# /*===========================================================
-sim_id <-
-  pi_data %>%
-  .[transfer == 1, ] %>%
-  .[, over_est := pi_diff_est - pi_diff] %>%
-  .[, o_diff := abs(over_est - 60)] %>%
-  .[which.min(o_diff), sim]
-
-il_data_oe <-
-  econ_data[sim == sim_id, ] %>%
-  .[transfer == 1, ]
-
-saveRDS(il_data_oe, here("Results/il_data_oe.rds"))
-
-# /*===========================================================
-#' # Find extreme cases of simulation results
+#' # Visually checking
 # /*===========================================================
 
-# ---------------------
-# aunit level results
-# ---------------------
-aunit_results <-
-  econ_data[, .(
-    sim, aunit_id, b0, b1, b2, Nk, opt_N,
-    b0_hat, b1_hat, b2_hat, opt_N_gwr,
-    transfer, pi_gwr, pi_gwr_est,
-    pi_diff, pi_diff_est
-  )] %>%
-  .[, lapply(.SD, mean), by = .(sim, transfer, aunit_id)] %>%
-  print()
-
-# ------------------------------------------------
-# find a simulation with "bad" GWR performances
-# ------------------------------------------------
-
-# === look at the percentag of positive N2 coefficient estimates
-sim_id <-
-  aunit_results %>%
-  .[, .(positive_count = sum(b2_hat > 0)), by = sim] %>%
-  .[order(-positive_count), ] %>%
-  .[1, sim]
-
-# === sim=530 seems to have the worst predicted N results ===#
-aunit_sim_single <- aunit_results[sim == sim_id, ]
-
-saveRDS(aunit_sim_single, here("Results/aunit_sim_single.rds"))
-
-# === single simulation results graphing ===#
-{
-  plot_data <-
-    aunit_sim_single %>%
-    .[, type := ifelse(transfer == 0, "GWR-R", "GWR-T")] %>%
-    .[type == "GWR-R", ] %>%
-    .[, .(aunit_id, b1, b2, b1_hat, b2_hat)] %>%
-    melt(id.var = "aunit_id") %>%
-    .[, type := ifelse(str_detect(variable, "hat"), "Estimated", "True")] %>%
-    .[, variable := gsub("_hat", "", variable)] %>%
-    dcast(aunit_id + variable ~ type, value.var = "value")
-
-  plot_data[variable == "b1", ] %>%
+left_join(field_sf, 
+          field_pars[sim==3,], 
+          by = c("cell_id","lon", "lat", "aunit_id")) %>% 
     ggplot(data = .) +
-    geom_point(aes(y = Estimated, x = True), size = 0.4) +
-    geom_abline(interept = 0, slope = 1, color = "red") +
-    xlim(0, NA) +
-    ylim(0, NA) +
-    coord_equal()
-
-  plot_data[variable == "b2", ] %>%
-    ggplot(data = .) +
-    geom_point(aes(y = Estimated, x = True), size = 0.4) +
-    geom_abline(interept = 0, slope = 1, color = "red") +
-    xlim(NA, 0.1) +
-    ylim(NA, 0.1) +
-    coord_equal()
-
-  aunit_sim_single %>%
-    .[, type := ifelse(transfer == 0, "GWR-R", "GWR-T")] %>%
-    .[type == "GWR-R", ] %>%
-    .[, .(aunit_id, opt_N, opt_N_gwr)] %>%
-    ggplot(data = .) +
-    geom_point(aes(y = opt_N_gwr, x = opt_N), size = 0.4) +
-    geom_abline(slope = 1, color = "red") +
-    xlab("True Optimal Nitrogen Rate (kg/ha)") +
-    ylab("Estimated Optimal Nitrogen Rate (kg/ha)")
-}
-
-
-
-# -------------------------------------
-# extremely over-estimated GWR profits
-# -------------------------------------
-
-# === simulations with extreme overestimation cases ===#
-extr_ids <- pi_data[pi_diff_est > 400, sim]
-extr_data <- econ_data[sim %in% extr_ids, ] %>%
-  .[transfer == 1, ] %>%
-  .[, case := "extremely overestimated cases"]
-
-# === simulations with normal cases ===#
-norm_ids <- pi_data[pi_diff_est < 25 & transfer == 1, sim] %>%
-  sample(., size = length(extr_ids), replace = FALSE)
-normal_data <- econ_data[sim %in% norm_ids, ] %>%
-  .[transfer == 1, ] %>%
-  .[, case := "normal cases"]
-
-# === compare extreme and normal cases ===#
-rbind(extr_data, normal_data) %>%
-  ggplot(
-    data = .,
-    aes(b1_hat, fill = factor(sim), color = factor(sim))
-  ) +
-  geom_density(alpha = 0.1) +
-  facet_wrap(~case, ncol = 2, scales = "free") +
-  ylab("density") +
-  xlab("b1_hat by transferred models") +
-  theme_bw() +
-  theme(
-    legend.position = "bottom",
-    legend.title = element_text(size = 12),
-    legend.text = element_text(size = 10),
-    axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5),
-    axis.text = element_text(color = "black")
-  )
-
-#* The over-estimated cases are associated with higher b1_hat estimates
-rbind(extr_data, normal_data) %>%
-  .[, .(
-    opt_N_scam = mean(opt_N_scam),
-    opt_N_gwr = mean(opt_N_gwr),
-    b1_hat = mean(b1_hat),
-    b2_hat = mean(b2_hat),
-    pi_diff_est = mean(pi_diff_est)
-  ),
-  by = .(sim, case)
-  ] %>%
-  print()
-
-
-# === the case with the most extreme over-estimation ===#
-df <- econ_data[sim == 795 & transfer == 1, ] %>%
-  .[transfer == 1, ]
-df[, b1_hat] %>% summary()
-df[, b2_hat] %>% unique()
-df[, b0_hat] %>% summary()
-df[, opt_N_scam] %>% unique()
-df[, opt_N_gwr] %>% summary()
-df %>%
-  .[, yield_gwr := gen_yield_QP(b0, b1, b2, Nk, 268)] %>%
-  .[, yield_gwr_est := gen_yield_QD(b0_hat, b1_hat, b2_hat, 268)] %>%
-  ggplot(data = ., aes(x = opt_N_gwr, y = yield_gwr_est)) +
-  geom_point()
-
-# === the reason for extreme over-estimation:
-# ===   super high or low opt_N_scam estimates, and
-# ===   the assumed quadratic GWR response functional form
-pi_data %>%
-  .[, type := ifelse(transfer == 0, "GWR-R", "GWR-T")] %>%
-  ggplot(data = ., aes(x = opt_N_scam, y = pi_diff_est, group = type)) +
-  geom_point() +
-  facet_wrap(~type, ncol = 2) +
-  xlab("Estimated optimal N rate by SCAM") +
-  ylab("GWR benefit, estimated")
+    geom_sf(aes(fill = EONR), size = 0.1) +
+    scale_fill_gradientn(colours = rev(terrain.colors(100))) +
+    theme_void()
+ggsave(here("Shared/Data/ToShare/example_field.png"),
+       height=4, width=7.2)
